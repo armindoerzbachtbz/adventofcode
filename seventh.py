faa5f5f92dba02daf927adbd05b08b20bcdda6c2
Armin Doerzbach <ado@doerzbach.com>
2023-12-07 17:44:50 +0100

    Moving docstrings at the right position

--- seventh.py
+++ seventh.py
@@ -1,10 +1,12 @@
 from operator import itemgetter
 
-'''
-This Card object implements a simple Card with the Symbol and value of the card
-'''
+
 class Card:
-    def __init__(self, symbol,value):
+    '''
+    This Card object implements a simple Card with the Symbol and value of the card
+    '''
+
+    def __init__(self, symbol, value):
         self.value = value
         self.symbol = symbol
 
@@ -14,10 +16,11 @@ class Card:
     def __repr__(self):
         return str(self.symbol)
 
-'''
-This is the static class containing all Card Objects.
-'''
+
 class Cards:
+    '''
+    This is the static class containing all Card Objects.
+    '''
     cards = {}
     cardvalues = {
         '2': 2,
@@ -35,16 +38,16 @@ class Cards:
         'A': 14
     }
     for k, v in cardvalues.items():
-        cards[k] = Card(k,v)
+        cards[k] = Card(k, v)
 
     def getcard(k):
         return Cards.cards[k]
 
 
-'''
-This Class implements all the stuff to compare and store a Hand.
-'''
 class Hand:
+    '''
+    This Class implements all the stuff to compare and store a Hand.
+    '''
 
     def setJoker():
         Cards.cards['J'].value = 1
@@ -61,13 +64,11 @@ class Hand:
             self.hand.append(Cards.getcard(symbol))
         self.uniquecards = set(self.hand)
 
-
-
-    '''
-    Checks if there are x similiar Cards.
-    if J is a Jocker this is also taken into account.
-    '''
     def isxofakind(self, x):
+        '''
+        Checks if there are x similiar Cards.
+        if J is a Jocker this is also taken into account.
+        '''
 
         if len(self.uniquecards) == 1 and x == 5:
             return True
@@ -103,20 +104,20 @@ class Hand:
                 countjoker += 1
         return countjoker
 
-    '''
-    Returns the typevalue of the hand which is
-   
-    7000000000000: Five of a kind, where all five cards have the same label: AAAAA
-    6000000000000: Four of a kind, where four cards have the same label and one card has a different label: AA8AA
-    5000000000000: Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
-    4000000000000: Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
-    3000000000000: Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
-    2000000000000: One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
-    1000000000000: High card, where all cards'
-    
-    
-    '''
     def typevalue(self):
+        '''
+        Returns the typevalue of the hand which is
+
+        7000000000000: Five of a kind, where all five cards have the same label: AAAAA
+        6000000000000: Four of a kind, where four cards have the same label and one card has a different label: AA8AA
+        5000000000000: Full house, where three cards have the same label, and the remaining two cards share a different label: 23332
+        4000000000000: Three of a kind, where three cards have the same label, and the remaining two cards are each different from any other card in the hand: TTT98
+        3000000000000: Two pair, where two cards share one label, two other cards share a second label, and the remaining card has a third label: 23432
+        2000000000000: One pair, where two cards share one label, and the other three cards have a different label from the pair and each other: A23A4
+        1000000000000: High card, where all cards'
+
+
+        '''
         if self.isxofakind(5):
             return int(7e12)
         elif self.isxofakind(4):
@@ -132,17 +133,17 @@ class Hand:
         else:
             return int(1e12)
 
-    '''
-    Returns the value of the Cards in the right order:
-    aabbccddee
-    
-    aa: is two digits for the first card of the hand
-    bb: is two digits for the second card of the hand
-    ...
-    
-    -> Comparing of the value is simple
-    '''
     def cardvalue(self):
+        '''
+        Returns the value of the Cards in the right order:
+        aabbccddee
+
+        aa: is two digits for the first card of the hand
+        bb: is two digits for the second card of the hand
+        ...
+
+        -> Comparing of the value is simple
+        '''
         cardvalue = 0
         i = 0
         while i < 5:
@@ -150,10 +151,10 @@ class Hand:
             i += 1
         return cardvalue
 
-    '''
-    Returns the value of the card which is the sum of the typevalue and the cardvalue.
-    '''
     def value(self):
+        '''
+        Returns the value of the card which is the sum of the typevalue and the cardvalue.
+        '''
         return self.typevalue() + self.cardvalue()
 
     def __lt__(self, other):
@@ -163,35 +164,36 @@ class Hand:
         return str([str(x) for x in self.hand] + [self.value()])
 
 
-hands = []
-with open("seventh.txt", "r") as f:
-    for line in f:
-        symbols, bid = line.strip().split()
-        hands.append([Hand(symbols), int(bid)])
+if __name__ == '__main__':
 
-Hand.unsetJoker()
-s1 = 0
-rank = 1
-sorted_hands = sorted(hands, key=itemgetter(0))
-for hand in sorted_hands:
-    s1 += rank * hand[1]
-    rank += 1
-print("Part1 ==============")
-print(sorted_hands[:5])
-print(sorted_hands[-5:])
-print("Part1 ==============")
+    hands = []
+    with open("seventh.txt", "r") as f:
+        for line in f:
+            symbols, bid = line.strip().split()
+            hands.append([Hand(symbols), int(bid)])
 
-Hand.setJoker()
-s = 0
-rank = 1
-sorted_hands_2 = sorted(hands, key=itemgetter(0))
-for hand in sorted_hands_2:
-    s += rank * hand[1]
-    rank += 1
-print("Part2 ==============")
-print(sorted_hands_2[:5])
-print(sorted_hands_2[-5:])
-print("Part2 ==============")
-print("Solution Part1: %d"%s1)
-print("Solution Part2: %d"%s)
+    Hand.unsetJoker()
+    s1 = 0
+    rank = 1
+    sorted_hands = sorted(hands, key=itemgetter(0))
+    for hand in sorted_hands:
+        s1 += rank * hand[1]
+        rank += 1
+    print("Part1 ==============")
+    print(sorted_hands[:5])
+    print(sorted_hands[-5:])
+    print("Part1 ==============")
 
+    Hand.setJoker()
+    s = 0
+    rank = 1
+    sorted_hands_2 = sorted(hands, key=itemgetter(0))
+    for hand in sorted_hands_2:
+        s += rank * hand[1]
+        rank += 1
+    print("Part2 ==============")
+    print(sorted_hands_2[:5])
+    print(sorted_hands_2[-5:])
+    print("Part2 ==============")
+    print("Solution Part1: %d" % s1)
+    print("Solution Part2: %d" % s)
